f29a96de8c4da8032bead2669afdf13f922ef9ae
Daniel Alejandro <136534872+Daniel-L10N@users.noreply.github.com>
2023-09-05 10:43:06 -0400

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
@@ -6,7 +7,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
      
-     <h1>Hola Rey, pagina en desarrollo</h1>
+     <h1 className="text-lg text-center">Dalitsof</h1>
+     <p><blockquote>softwrare daniel</blockquote></p>
+     <p><blockquote>daniel softwarw</blockquote></p>
+     <blockquote>Dalitsof</blockquote>
     </main>
   );
 }
